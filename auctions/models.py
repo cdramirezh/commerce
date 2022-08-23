a4ac96d2ee65cddcbc014aed981aa5195d285c97
higi <cdramirezh@unal.edu.co>
2022-08-22 21:08:02 -0500

    Added auctions model. Added model to Admin. Auction Model to be completed

--- auctions/models.py
+++ auctions/models.py
@@ -7,3 +7,16 @@ class User(AbstractUser):
 
     def __str__(self):
         return f'{self.username}'
+
+class Auction(models.Model):
+    title = models.CharField(max_length=64)
+    # description
+    # creationDate
+    # image
+    # category
+    # currentPrice
+    # active
+    # winner
+
+    def __str__(self):
+        return f'{self.title}'
