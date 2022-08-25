0e14909b4c6167086af222eb384e9ad7dc10ac81
higi <cdramirezh@unal.edu.co>
2022-08-25 15:44:40 -0500

    Added Watchlist to the user

--- auctions/models.py
+++ auctions/models.py
@@ -4,7 +4,8 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+
+    watchlist = models.ManyToManyField('Auction', blank=True)
 
     def __str__(self):
         return f'{self.username}'
